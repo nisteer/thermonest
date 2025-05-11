ffde8ccf4d4d0835a5ff8f79583350fc678ac3ce
nisteer <80920005+nisteer@users.noreply.github.com>
2025-05-11 14:21:01 +0200

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -5,4 +5,8 @@ module.exports = {
     org: process.env.INFLUX_ORG,
     bucket: process.env.INFLUX_BUCKET,
   },
+  auth0: {
+    domain: process.env.AUTH0_DOMAIN,
+    audience: process.env.AUTH0_AUDIENCE,
+  },
 };
